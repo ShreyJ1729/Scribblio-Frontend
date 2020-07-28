3c865af9cefb83921ae796fc0326e42136af631e
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-28 18:13:17 -0500

    Updates

---
+++ precache-manifest.b9a77ba3bdf2bb8bc5ebeceb178988e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a0fb68469ae169c799094832a91d81",
+    "url": "/Scribblio-Frontend/index.html"
+  },
+  {
+    "revision": "9af10745b2ff42fe7c9a",
+    "url": "/Scribblio-Frontend/static/js/2.a8038533.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/Scribblio-Frontend/static/js/2.a8038533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a7f5dc377ee2d51f71",
+    "url": "/Scribblio-Frontend/static/js/main.7c7c420f.chunk.js"
+  },
+  {
+    "revision": "20cf9439abc7d0267299",
+    "url": "/Scribblio-Frontend/static/js/runtime-main.69412044.js"
+  }
+]);
