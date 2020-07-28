b278d3c5c4118141bb9c44c061f033c6191764ef
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-28 18:19:22 -0500

    made server url https

--- src/socket-api.js
+++ src/socket-api.js
@@ -1,4 +1,4 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://scribblio.herokuapp.com/");
+const socket = openSocket("https://scribblio.herokuapp.com/");
 // const socket = openSocket("localhost:8000");
 export default socket;
